7491d778b49d9f54e3932b9a50df2ddb424f1160
DONNY <dony@tapy.org>
2020-08-01 14:30:03 +0800

    update acc api secret for test env.

--- config/config.test.js
+++ config/config.test.js
@@ -38,10 +38,10 @@ export default {
     routes: pageRoutes,
     define: {
         "APP_TYPE": process.env.APP_TYPE || '',
-        "process.env.API_GATEWAY":'http://acc.api.kuga.wang/v3/gateway',
+        "process.env.API_GATEWAY":'https://acc.api.depoga.net/v3/gateway',
         "process.env.API_KEY": 1000,
         "process.env.TITLE":'ACC.TEST',
-        "process.env.API_SECRET":''
+        "process.env.API_SECRET":'04ebe033d4efb4c9e90d543d5167699b'
     },
     lessLoaderOptions: {
         javascriptEnabled: true,
